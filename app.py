892fdcbc579003ece6135df2af17200cf28e612a
MitsukiUsui <mitsuo.com.mu@gmail.com>
2022-01-17 12:24:37 +0900

    use dcc.Stroe

--- app.py
+++ app.py
@@ -92,7 +92,9 @@ app.layout = html.Div([
             ),
         ],
         style={'display': 'flex', 'flex-direction': 'row'}
-    )
+    ),
+
+    dcc.Store(id='filtered-rookie-json')
 ])
 
 
@@ -115,24 +117,31 @@ def filter_rookie_df(df, joined_teams=None, prev_teams=None, player_names=None,
 
 
 @app.callback(
-    Output('player-table', 'data'),
-    Output('player-table', 'page_count'),
+    Output('filtered-rookie-json', 'data'),
     Input('joined-team-dropdown', 'value'),
     Input('prev-team-dropdown', 'value'),
     Input('joined-league-dropdown', 'value'),
     Input('prev-category-dropdown', 'value'),
-    Input('position-dropdown', 'value'),
-    Input('player-table', 'page_current'),
-    Input('page-size-input', 'value'),
+    Input('position-dropdown', 'value')
 )
-def update_player_table(joined_teams, prev_teams, joined_league_ids, prev_categories, positions,
-                        page_current, page_size):
+def update_matched_rookie_df(joined_teams, prev_teams, joined_league_ids, prev_categories, positions):
     f_rookie_df = filter_rookie_df(rookie_df, joined_teams=joined_teams, prev_teams=prev_teams,
                                    joined_league_ids=joined_league_ids,
                                    prev_categories=prev_categories, positions=positions)
+    return f_rookie_df.to_json(date_format='iso', orient='split')
+
+
+@app.callback(
+    Output('player-table', 'data'),
+    Output('player-table', 'page_count'),
+    Input('filtered-rookie-json', 'data'),
+    Input('player-table', 'page_current'),
+    Input('page-size-input', 'value'),
+)
+def update_player_table(filtered_rookie_json, page_current, page_size):
+    f_rookie_df = pd.read_json(filtered_rookie_json, orient='split')
     records = f_rookie_df.iloc[page_current * page_size:(page_current + 1) * page_size].to_dict('records')
     page_count = math.ceil(len(f_rookie_df) / page_size)
-
     return [records, page_count]
 
 
@@ -144,7 +153,6 @@ def update_player_graph(rows):
     player_names = [row['player_name'] for row in rows]
     f_rookie_df = filter_rookie_df(rookie_df, player_names=player_names)
     f_stats_df = pd.merge(stats_df, f_rookie_df['player_name'], on='player_name')
-
     fig = do_scatter_plot_play_time(
         f_rookie_df,
         f_stats_df,
@@ -157,22 +165,20 @@ def update_player_graph(rows):
 
 @app.callback(
     Output('player-count-graph-container', 'children'),
-    Input('joined-team-dropdown', 'value'),
-    Input('prev-team-dropdown', 'value')
+    Input('filtered-rookie-json', 'data')
 )
-def update_player_count_graph(selected_joined_teams, selected_prev_teams):
-    f_rookie_df = filter_rookie_df(rookie_df, selected_joined_teams, selected_prev_teams)
+def update_player_count_graph(filtered_rookie_json):
+    f_rookie_df = pd.read_json(filtered_rookie_json, orient='split')
     fig = do_bar_plot_player_count(f_rookie_df)
     return [dcc.Graph(id='player-count-graph', figure=fig)]
 
 
 @app.callback(
     Output('avg-player-graph-container', 'children'),
-    Input('joined-team-dropdown', 'value'),
-    Input('prev-team-dropdown', 'value')
+    Input('filtered-rookie-json', 'data')
 )
-def update_avg_player_graph(selected_joined_teams, selected_prev_teams):
-    f_rookie_df = filter_rookie_df(rookie_df, selected_joined_teams, selected_prev_teams)
+def update_avg_player_graph(filtered_rookie_json):
+    f_rookie_df = pd.read_json(filtered_rookie_json, orient='split')
     f_stats_df = pd.merge(stats_df, f_rookie_df['player_name'], on='player_name')
     fig = do_scatter_plot_avg_play_time(f_rookie_df, f_stats_df)
     return [dcc.Graph(id='avg-player-graph', figure=fig)]
@@ -180,12 +186,11 @@ def update_avg_player_graph(selected_joined_teams, selected_prev_teams):
 
 @app.callback(
     Output('play-time-histogram-container', 'children'),
-    Input('joined-team-dropdown', 'value'),
-    Input('prev-team-dropdown', 'value'),
+    Input('filtered-rookie-json', 'data'),
     Input('rookie-year-input', 'value'),
 )
-def update_play_time_histogram(selected_joined_teams, selected_prev_teams, rookie_year):
-    f_rookie_df = filter_rookie_df(rookie_df, selected_joined_teams, selected_prev_teams)
+def update_play_time_histogram(filtered_rookie_json, rookie_year):
+    f_rookie_df = pd.read_json(filtered_rookie_json, orient='split')
     f_stats_df = pd.merge(stats_df, f_rookie_df['player_name'], on='player_name')
     f_stats_df = f_stats_df[f_stats_df['rookie_year'] == rookie_year]
     fig = do_histogram_play_time(f_stats_df)
